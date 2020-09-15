f5ed782cbf2af30df52b75a88b50ea2bddf7e88e
Luckylake215 <70714955+Luckylake215@users.noreply.github.com>
2020-09-15 13:01:10 +0300

    Add files via upload

---
+++ TestHuman.java
@@ -0,0 +1,11 @@
+package HomoSapiens;
+
+public class TestHuman {
+
+    public TestHuman(String round, String aLong, String strong) {
+    }
+
+    public static void main(String[] args){
+        TestHuman testhuman =new TestHuman("round","long","strong");
+    }
+}
